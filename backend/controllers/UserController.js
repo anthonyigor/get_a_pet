d5fa687349d5fe797115bcd83df880bb699dd687
Anthony Igor <igor.torrestj11@gmail.com>
2023-07-25 18:40:59 -0300

    helper verify user token

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -127,4 +127,11 @@ module.exports = class UserController {
         res.status(200).json({user})
 
     }
+
+    static async editUser(req, res) {
+        res.status(200).json({message: 'Update realizado'})
+
+        return
+    }
+
 }
