41a7b435dc7aba636517ff2e588cacfe3482f653
Anthony Igor <igor.torrestj11@gmail.com>
2023-08-02 19:38:06 -0300

    update pet

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -1,3 +1,4 @@
+const { Aggregate } = require('mongoose')
 const getToken = require('../helpers/get-token')
 const getUserByToken = require('../helpers/get-user-by-token')
 const ObjectId = require('mongoose').Types.ObjectId
@@ -125,6 +126,7 @@ module.exports = class PetController {
 
         if (!pet) {
             res.status(404).json({message: 'Pet não encontrado'})
+            return
         }
 
         // check if user logged is the pet owner
@@ -133,11 +135,95 @@ module.exports = class PetController {
         
         if(String(pet.user._id) !== String(user._id)) {
             res.status(422).json({message: 'Erro ao processar solicitação. Não é possível remover um pet de outro usuário!'})
+            return
         }
 
         await Pet.findByIdAndDelete(id)
         res.status(200).json({message: 'Pet removido com sucesso!'})
 
+    }
+
+    static async editPet(req, res) {
+        const id = req.params.id
+
+        const {name, age, weight, color, available} = req.body
+        const images = req.files
+
+        const updatedData = {}
+
+        // check if the id is a valid ObjecId
+        if (!ObjectId.isValid(id)) {
+            res.status(422).json({message: "ID inválido!"})
+            return
+        }
+
+        // check if pet exists
+        const pet = await Pet.findById(id)
+        if (!pet) {
+            res.status(404).json({message: 'Pet não encontrado'})
+            return
+        }
+
+        // check if user logged is the pet owner
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+        
+        if(String(pet.user._id) !== String(user._id)) {
+            res.status(422).json({message: 'Erro ao processar solicitação. Não é possível editar um pet de outro usuário!'})
+            return
+        }
+
+        // validations
+        if (!name) {
+            res.status(422).json({message: "O nome é obrigatório!"})
+            return
+        } else {
+            updatedData.name = name
+        }
+
+        if (!age) {
+            res.status(422).json({message: "A idade é obrigatória!"})
+            return
+        } else {
+            updatedData.age = age
+        }
+        
+        if (!weight) {
+            res.status(422).json({message: "O peso é obrigatório!"})
+            return
+        } else {
+            updatedData.weight = weight
+        }
+
+        if (!color) {
+            res.status(422).json({message: "A cor é obrigatória!"})
+            return
+        } else {
+            updatedData.color = color
+        }
+
+        if (images.length === 0) {
+            res.status(422).json({message: "A imagem é obrigatória!"})
+            return
+        } else {
+            updatedData.images = []
+            images.map((image) => {
+                updatedData.images.push(image.filename)
+            })
+        }
+
+        if (pet.adopter) {
+            updatedData.available = false
+        } 
+
+        // update pet with the new data
+        try {
+            await Pet.findByIdAndUpdate(id, updatedData)
+            res.status(200).json({message: 'Pet atualizado com sucesso'})
+        } catch (error) {
+            res.status(500).json({message: error})
+            return
+        }
     }
 
 }
--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -7,9 +7,10 @@ const verifyToken = require('../helpers/verify-token')
 const {imageUpload} = require('../helpers/image-upload')
 
 router.get('/', PetController.getPets)
-router.get('/:id', PetController.getPet)
 router.post('/create', verifyToken, imageUpload.array('images'), PetController.createPet)
+router.get('/:id', PetController.getPet)
 router.delete('/:id', verifyToken, PetController.deletePet)
+router.patch('/:id', verifyToken, imageUpload.array('images'), PetController.editPet)
 router.get('/mypets', verifyToken, PetController.getUserPets)
 router.get('/myadoptions', verifyToken, PetController.getUserAdoptions)
 
