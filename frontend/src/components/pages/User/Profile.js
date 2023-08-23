90abfed32348994c0c18c2e022e649d272c34854
Anthony Igor <igor.torrestj11@gmail.com>
2023-08-23 18:53:00 -0300

    rounded image

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -6,15 +6,18 @@ import styles from './Profile.module.css'
 
 import Input from '../../form/Input'
 import useFlashMessage from '../../../hooks/useFlashMessage'
+import RoundedImage from '../../layout/RoundedImage'
 
 function Profile() {
 
     const [user, setUser] = useState({})
+    const [preview, setPreview] = useState()
     const [token] = useState(localStorage.getItem('token') || '')
     const {setFlashMessage} = useFlashMessage()
 
     useEffect(() => {
-    
+        
+        // get current user by token
         api.get('/users/checkuser', {
             headers: {
                 Authorization: `Bearer ${JSON.parse(token)}`
@@ -31,6 +34,9 @@ function Profile() {
     }
 
     function onFileChange(e) {
+        // set the preview as the image uploaded
+        setPreview(e.target.files[0])
+
         setUser({...user, [e.target.name]: e.target.files[0]})      
     }
 
@@ -41,8 +47,10 @@ function Profile() {
 
         const formData = new FormData()
 
+        // fills formData with user data from the form
         await Object.keys(user).forEach((key) => formData.append(key, user[key]))
 
+        // axios api call send formData
         const data = await api.patch(`/users/edit/${user._id}`, formData, {
             headers: {
                 Authorization: `Bearer ${JSON.parse(token)}`,
@@ -64,7 +72,14 @@ function Profile() {
         <section>
             <div className={styles.profile_header}>
                 <h1>Perfil</h1>
-                <p>Preview Imagem</p>
+                {(user.image || preview) && (
+                    <RoundedImage 
+                        src={
+                        preview ? URL.createObjectURL(preview) : `${process.env.REACT_APP_API}/images/users/${user.image}`} 
+                        alt={user.name}
+                        />
+                )}
+
             </div>
             <form onSubmit={handleSubmit} className={formStyles.form_container}>
                 <Input
