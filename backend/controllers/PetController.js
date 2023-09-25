cb35b07a66bdcfbe70b452e8fcabaacef99378c4
Anthony Igor <igor.torrestj11@gmail.com>
2023-09-25 19:34:04 -0300

    myPets api requests / backend routes correction

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -78,8 +78,8 @@ module.exports = class PetController {
         const token = getToken(req)
         const user = await getUserByToken(token)
 
-        const userPets = await Pet.find({'user._id': user._id}).sort('-createdAt')
-        res.status(200).json({userPets: userPets})
+        const pets = await Pet.find({'user._id': user._id}).sort('-createdAt')
+        res.status(200).json({pets,})
 
     }
 
