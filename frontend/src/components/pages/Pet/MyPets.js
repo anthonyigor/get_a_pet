a0e8a799c08afdb1ace2b9d327b69195f8d69339
Anthony Igor <igor.torrestj11@gmail.com>
2023-09-25 19:53:53 -0300

    show pets in myPets dashboard

--- frontend/src/components/pages/Pet/MyPets.js
+++ frontend/src/components/pages/Pet/MyPets.js
@@ -2,6 +2,8 @@ import api from '../../../utils/api'
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
+import styles from './Dashboard.module.css'
+
 import useFlashMessage from '../../../hooks/useFlashMessage'
 import RoundedImage from '../../layout/RoundedImage'
 
@@ -26,7 +28,31 @@ function MyPets() {
             <h1>MyPets</h1>
             <Link to='/pet/add'>Cadastrar pet</Link>
             <div>
-                {pets.length > 0 && <p>Meus pets cadastrados</p>}
+                {pets.length > 0 && 
+                    pets.map((pet) => (
+                        <div key={pet._id}>
+                            <RoundedImage
+                                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`} 
+                                alt={pet.name}
+                                width="75px"
+                            />
+                            <span className='bold'>{pet.name}</span>
+                            <div className={styles.action}>
+                                {pet.available ? 
+                                (<>
+                                    {pet.adopter && (
+                                        <button>Concluir adoção</button>
+                                    )}
+                                    <Link to={`pet/edit/${pet._id}`}>Editar</Link>
+                                    <button>Excluir</button>
+                                </>)
+                                : (
+                                    <p>Pet já adotado</p>
+                                )}
+                            </div>
+                        </div>
+                    ))
+                }
                 {pets.length === 0 && <p>Não há pets cadastrados</p>}
             </div>
         </section>
